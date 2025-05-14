f69141d49a662ca21dec2b64d6f2a57d5d8f8824
Erick Galinkin <erick.galinkin@gmail.com>
2025-05-13 21:20:47 -0400

    Add specialized calculators for Ransomware and Cryptominer defense

--- Debugged_CybORG/CybORG/CybORG/Shared/BlueRewardCalculator.py
+++ Debugged_CybORG/CybORG/CybORG/Shared/BlueRewardCalculator.py
@@ -1,11 +1,12 @@
 from collections import namedtuple
 
 from CybORG.Shared import Scenario
-from CybORG.Shared.RedRewardCalculator import DistruptRewardCalculator, PwnRewardCalculator
+from CybORG.Shared.RedRewardCalculator import (DistruptRewardCalculator, PwnRewardCalculator,
+                                               RansomwareRewardCalculator, CryptominerRewardCalculator)
 from CybORG.Shared.RewardCalculator import RewardCalculator
 
-
 HostReward = namedtuple('HostReward','confidentiality availability')
+
 
 class ConfidentialityRewardCalculator(RewardCalculator):
     # Calculate punishment for defending agent based on compromise of hosts/data
@@ -51,6 +52,7 @@ class AvailabilityRewardCalculator(RewardCalculator):
         for host, value in self.disrupt_rc.impacted_hosts.items():
             self.impacted_hosts[host] = -1 * value
 
+
 class HybridAvailabilityConfidentialityRewardCalculator(RewardCalculator):
     # Hybrid of availability and confidentiality reward calculator
     def __init__(self, agent_name: str, scenario: Scenario):
@@ -80,3 +82,74 @@ class HybridAvailabilityConfidentialityRewardCalculator(RewardCalculator):
             reward_state = HostReward(compromised,impacted)  
                                     # confidentiality, availability
             self.host_scores[host] = reward_state
+
+
+class RansomwareDefenseCalculator(RewardCalculator):
+    # Specialized calculator for computing ransomware defensive rewards
+    def __init__(self, agent_name: str, scenario: Scenario):
+        super(RansomwareDefenseCalculator, self).__init__(agent_name)
+        self.default_calc = HybridAvailabilityConfidentialityRewardCalculator(agent_name, scenario)
+        self.ransomware_calc = RansomwareRewardCalculator(agent_name, scenario)
+        self.compromised_hosts = dict()
+
+    def reset(self):
+        self.default_calc.reset()
+        self.ransomware_calc.reset()
+        self.compromised_hosts = dict()
+
+    def _compute_host_scores(self, hostnames):
+        self.default_calc._compute_host_scores(hostnames)
+        self.host_scores = self.default_calc.host_scores
+        self.compromised_hosts = self.ransomware_calc.compromised_hosts
+        for host in hostnames:
+            if host == 'success':
+                continue
+            if host in self.compromised_hosts.keys():
+                compromised = compromised_hosts[host] if host in compromised_hosts else 0
+                self.host_scores[host].confidentiality = self.compromised_hosts[host].confidentiality - compromised
+                self.host_scores[host].availability = self.compromised_hosts[host].availability - compromised
+
+    def calculate_reward(self, current_state: dict, action: dict, agent_observations: dict, done: bool) -> float:
+        reward = (self.default_calc.calculate_reward(current_state, action, agent_observations, done) -
+                  self.calculate_ransomware_reward(current_state, action, agent_observations, done))
+        self._compute_host_scores(current_state.keys())
+        return reward
+
+    def calculate_ransomware_reward(self, current_state: dict, action: dict, agent_observations: dict, done: bool) -> float:
+        reward = -self.ransomware_calc.calculate_reward(current_state, action, agent_observations, done)
+        return reward
+
+
+class CryptominerDefenseCalculator(RewardCalculator):
+    # Specialized calculator for computing cryptominer defensive rewards
+    def __init__(self, agent_name: str, scenario: Scenario):
+        super(CryptominerDefenseCalculator, self).__init__(agent_name)
+        self.default_calc = HybridAvailabilityConfidentialityRewardCalculator(agent_name, scenario)
+        self.cryptominer_calc = CryptominerRewardCalculator(agent_name, scenario)
+        self.compromised_hosts = dict()
+
+    def reset(self):
+        self.default_calc.reset()
+        self.ransomware_calculator.reset()
+        self.compromised_hosts = dict()
+
+    def _compute_host_scores(self, hostnames):
+        self.default_calc._compute_host_scores(hostnames)
+        self.host_scores = self.default_calc.host_scores
+        self.compromised_hosts = self.ransomware_calculator.compromised_hosts
+        for host in hostnames:
+            if host == 'success':
+                continue
+            if host in self.compromised_hosts.keys():
+                compromised = compromised_hosts[host] if host in compromised_hosts else 0
+                self.host_scores[host].confidentiality = self.compromised_hosts[host].confidentiality - compromised
+
+    def calculate_reward(self, current_state: dict, action: dict, agent_observations: dict, done: bool) -> float:
+        reward = (self.default_calc.calculate_reward(current_state, action, agent_observations, done) -
+                  self.calculate_cryptominer_reward(current_state, action, agent_observations, done))
+        self._compute_host_scores(current_state.keys())
+        return reward
+
+    def calculate_cryptominer_reward(self, current_state: dict, action: dict, agent_observations: dict, done: bool) -> float:
+        reward = -self.cryptominer_calc.calculate_reward(current_state, action, agent_observations, done)
+        return reward
